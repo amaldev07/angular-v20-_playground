0c55764b9ddd7cf4384e850afec4c094de9e31e8
Amaldev <amaldev.psn@gmail.com>
2026-02-26 23:53:18 +0530

    SingalComponent

--- src/app/singal-component/singal-component.ts
+++ src/app/singal-component/singal-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-singal-component',
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./singal-component.scss'],
 })
 export class SingalComponent {
+  quantity = signal(0);
+  price = signal(0);
+  total = computed(() => {
+    debugger;
+    return this.quantity() * this.price();
+  });
+  x: any;
+  constructor() {
+    this.x = effect(() => {
+      console.log('quantity changed:', this.quantity());
+      console.log('price changed:', this.price());
+    });
+  }
 
-}
+  ngOnInit() { }
+  incriment() {
+    this.quantity.update(v => v + 1);
+  }
+  incrimentPrice() {
+    this.price.update(v => v + 1);
+  }
+} 
